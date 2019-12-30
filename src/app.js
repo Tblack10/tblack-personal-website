73cd44b6788d9b6b44ac05986f7eedad54357b56
Travis Black <Travis_Black@Live.com>
2019-12-29 19:58:50 -0800

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//handlebars initialization
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//static directory init
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Welcome!',
+        name: 'Travis Black'
+    })
+})
+
+app.get('/resume', (req, res) => {
+    res.render('resume', {
+        title: 'Resume',
+        name: 'Travis Black',
+        resumeMessage: 'This is my resume'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Travis Black',
+    })
+})
+
+app.get('/privacyPolicy', (req, res) => {
+    res.render('privacyPolicy', {
+        title: 'Privacy Policy',
+        name: 'Travis Black'
+    })
+})
+
+app.get('/applications', (req, res) => {
+    res.render('applications', {
+        title: 'Applications',
+        name: 'Travis Black'
+    })
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact', {
+        title: 'Contact Me',
+        name: 'Travis Black'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
