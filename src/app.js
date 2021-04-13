e18d8cd498677067e089a93aa6b709b4b0c74fee
tblack10 <travis_black@live.com>
2021-04-13 12:17:44 -0700

    Created tictactoe and Arthaiku about pages, style changes

--- src/app.js
+++ src/app.js
@@ -22,47 +22,47 @@ app.get('', (req, res) => {
     res.render('index', {
         title: 'Travis Black',
         name: 'Travis Black',
-        subtitle: 'Junior iOS Developer'
-    })
-})
-
-app.get('/resume', (req, res) => {
-    res.render('resume', {
-        title: 'Resume',
-        name: 'Travis Black',
-        resumeMessage: 'This is my resume'
-    })
-})
-
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Travis Black'
+        subtitle: 'Junior Software Developer'
     })
 })
 
 app.get('/privacyPolicy', (req, res) => {
     res.render('privacyPolicy', {
-        title: 'Privacy Policy',
-        name: 'Travis Black'
+        title: 'Travis Black',
+        name: 'Travis Black',
+        subtitle: 'Privacy Policy'
     })
 })
 
 app.get('/applications', (req, res) => {
     res.render('applications', {
-        title: 'My Apps',
-        name: 'Travis Black'
+        title: 'Travis Black',
+        name: 'Travis Black',
+        subtitle: 'My Apps'
     })
 })
 
 app.get('/contact', (req, res) => {
     res.render('contact', {
-        title: 'Contact Me',
+        title: 'Travis Black',
         name: 'Travis Black',
         email: 'travisb.dev@gmail.com',
         github: 'https://github.com/Tblack10',
-        linkedin: 'www.linkedin.com/in/travis--black',
-        appStore: 'https://apps.apple.com/us/developer/travis-black/id1445267343'
+        linkedin: 'https://www.linkedin.com/in/travis--black/',
+        appStore: 'https://apps.apple.com/ca/developer/travis-black/id1445267343',
+        subtitle: 'Contact Me'
+    })
+})
+
+app.get('/applications/elementaryTicTacToe', (req, res) => {
+    res.render('elementaryTicTacToe', {
+        name: 'Travis Black'
+    })
+})
+
+app.get('/applications/artHaikuVancouver', (req, res) => {
+    res.render('artHaiku', {
+        name: 'Travis Black'
     })
 })
 
